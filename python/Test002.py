c05169d42397ec461c5ef1f99aa018392a9bf275
Hyun <hickee32@gmail.com>
2023-12-30 17:40:32 +0900

    Add files via upload

---
+++ python/Test002.py
@@ -0,0 +1,18 @@
+# 변수 사용
+print('안녕')
+#이름을 붙여주는 방법중 하나라고 보자
+
+identity = '지구인'
+number_of_leg = 4
+print('인간은 다리가 4개가 존재한다')
+print(identity, '은 다리가', number_of_leg, '개가 존재한다')
+
+identity = '한국인'
+number_of_leg = 4
+print(identity, '은 다리가', number_of_leg, '개가 존재한다')
+
+#name이라는 변수를 만들고 거기에 자신의 이름을 넣어 보세요.
+# 이름을 넣을때는 이름을 따옴표로 감싸주어야 합니다.
+
+name = '지구인'
+print(name)
