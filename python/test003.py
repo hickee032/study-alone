19aa290b7ccb34b3004c4ece22669a0aac0b8937
Hyun <hickee32@gmail.com>
2024-01-01 23:06:21 +0900

    Add files via upload

---
+++ python/test003.py
@@ -0,0 +1,18 @@
+my_name = 'python'
+my_age = 30
+print(my_name, '은 이제', my_age, '살')
+
+my_next_age = my_age+1
+print('내년에는', my_next_age,'살')
+
+multiply = 9*9
+divide = 30/5
+power = 2 ** 10
+reminder = 15%3
+print(multiply,divide, power,reminder)
+
+text = '2015'+'1991'
+number = 2015+1991
+
+print(text)
+print(number)
