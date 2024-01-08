30cfcd4650b4632588d95b1b35f28fae8a0525b2
Hyun <hickee32@gmail.com>
2024-01-08 23:50:05 +0900

    Add files via upload

---
+++ CS_inflearn/Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_pr {
+    internal class Array_pr {
+        static void Main(string[] args) {
+
+            int[] array1 = new int[3];
+            array1[0] = 10;
+            array1[1] = 20;
+            array1[2] = 30;
+
+            int[] array2 = new int[] {1,2,3};
+            int[] array3 = { 4, 5, 6 };
+
+            Console.WriteLine(array1[0]);
+
+            Console.WriteLine(array2);
+
+            Console.WriteLine(array3);
+
+            for (int i = 0; i < array1.Length; i++) {
+                Console.WriteLine(array1[i]);
+            }
+
+            Console.WriteLine("array3");
+
+            foreach (int i in array3) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ CS_inflearn/Arraylist.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraylist_pr {
+    internal class Program {
+        static void Main(string[] args) {
+
+            ArrayList al = new ArrayList();
+
+            //추가
+            al.Add(1);
+            al.Add("hello");
+            al.Add(3.3);
+            al.Add(true);
+
+            //출력
+            foreach (var item in al) {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            //삭제
+            al.Remove("hello");
+
+            foreach (var item in al) {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
