364f320b74a696a481bf8f6021ea67418cd5d517
Hyun <hickee32@gmail.com>
2024-01-09 21:51:36 +0900

    Add files via upload

---
+++ CS_inflearn/Try_Catch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch {
+    internal class Program {
+        static void Main(string[] args) {
+            /*
+            Console.WriteLine("나눌 숫자를 입력");
+            int div = int.Parse(Console.ReadLine());
+            Console.WriteLine(10 / div);
+            //예외가 발생함
+            */
+
+            Console.WriteLine("나눌 숫자를 입력");
+            int div = int.Parse(Console.ReadLine());
+
+            try {
+                Console.WriteLine(10 / div);
+            }
+            catch(Exception e) {
+                Console.WriteLine("0으로 나눌수 없음"); ;
+                Console.WriteLine("예외메세지 - " + e.Message);
+            }
+           
+        }
+    }
+}
